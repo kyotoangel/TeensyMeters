8e75c48dfee1c5e7e74df469f00446978dbf3d50
kyotoangel <antonin.roux@insa-lyon.fr>
2025-09-06 00:43:23 +0200

    finally added sampler ! (still in dev)

--- src/include/sampler.h
+++ src/include/sampler.h
@@ -1,4 +1,23 @@
 #ifndef SAMPLER_H
 #define SAMPLER_H
 
+#include <Arduino.h>
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SerialFlash.h>
+
+class Sampler {
+    public :
+    Sampler();
+    void begin();
+    bool available();
+    void getLogBands(float *bands, int nBands);
+
+    private :
+    AudioInputI2S           i2s1;
+    AudioAnalyzeFFT1024     fft1024;
+    AudioConnection*        patchCord;
+    AudioControlSGTL5000    sgtl5000_1;
+};
 #endif
--- src/sampler.cpp
+++ src/sampler.cpp
@@ -1,1 +1,24 @@
 #include <include/sampler.h>
+
+Sampler::Sampler() {
+    patchCord = new AudioConnection(i2s1, 0, fft1024, 0);
+}
+
+void Sampler::begin() {
+    AudioMemory(12); // nombre de blocs (1 bloc = 128 samples)
+
+    sgtl5000_1.enable();
+    sgtl5000_1.inputSelect(AUDIO_INPUT_LINEIN);
+    sgtl5000_1.volume(0.5); // volume max = 1
+
+    fft1024.windowFunction(AudioWindowHanning1024); // lissage
+}
+
+bool Sampler::available() {
+    return fft1024.available();
+}
+
+// Renvoie nBands bandes log (dans le style de l'eq eight)
+void Sampler::getLogBands(float *bands, int nBands) {
+    
+}
